e638bd117d4d3513817d2386804eaefd0fb27c15
Dakota <bazookaharwell@gmail.com>
2017-04-19 11:56:39 -1000

    Create wordFilters hash table in routes

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
+const wordFilters = {
+  'selfie'    :   'self-portrait',
+  'yummers'   :   'delicious',
+  'outchea'   :   'are out here',
+  'bruh'      :   'wow',
+  'doge'      :   'pug',
+  'cilantro'  :   'soap',
+  'bae'       :   'loved one',
+  'swag'      :   'style',
+  'yolo'      :   'carpe diem'
+};
+
 // If an empty URI GET is requested
 // serve index.html
 router.route('/')
