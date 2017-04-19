37fbbb4713043587b3221b2ece476d636cb6c52e
Dakota <bazookaharwell@gmail.com>
2017-04-19 11:53:43 -1000

    Initialize message routes

---
+++ routes/message.js
@@ -0,0 +1,12 @@
+/*jshint esversion:6*/
+const express = require('express');
+const router = express.Router();
+
+// If an empty URI GET is requested
+// serve index.html
+router.route('/')
+      .post((req, res) => {
+        // TODO
+      });
+
+module.exports = router;
