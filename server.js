9bb5ec4db57b7d4031e7eb0063da7516898cf05e
Dakota <bazookaharwell@gmail.com>
2017-04-19 11:52:33 -1000

    Initialize server file

---
+++ server.js
@@ -0,0 +1,21 @@
+/*jshint esversion:6*/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const indexRoutes = require('./routes/message.js');
+
+// parse application/x-www-form-urlencoded
+// attach to req.body
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Routes
+app.use('/message', messageRoutes);
+
+// Initialize server
+const server = app.listen(3000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Server listening at http://%s:%s', host, port);
+});
