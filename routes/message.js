b02a178918e7b95282bcaa4745e7a2eb4eafa8f1
Dakota <bazookaharwell@gmail.com>
2017-04-19 12:13:04 -1000

    Stub out filterWords function

--- routes/message.js
+++ routes/message.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const wordFilters = {
+const WORDFILTERS = {
   'selfie'    :   'self-portrait',
   'yummers'   :   'delicious',
   'outchea'   :   'are out here',
@@ -14,11 +14,27 @@ const wordFilters = {
   'yolo'      :   'carpe diem'
 };
 
-// If an empty URI GET is requested
-// serve index.html
+/** function filterWords
+  * Parameters:
+  *   String to be filtered
+  * Return value:
+  *   A filtered string according to
+  *   the WORDFILTERS object listed at the top
+  *   of this document.
+  */
+function filterWords(str) {
+  const filterKeys = Object.keys(WORDFILTERS);
+  const filterValues = Object.values(WORDFILTERS);
+  for (let i = 0; i < filterKeys.length; ++i) {
+    const re = new RegExp('regex', `/\\b${filterKeys[i]}\\b/gi`);
+    str.replace(re, filterValues[i]);
+  }
+  return str;
+}
+
 router.route('/')
       .post((req, res) => {
-        // TODO
+        res.send(filterWords(req.body.message));
       });
 
 module.exports = router;
